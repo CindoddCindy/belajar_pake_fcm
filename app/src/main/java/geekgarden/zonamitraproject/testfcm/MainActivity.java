27d9006a3b7656e2ce3df491149157bd45c010ea
CindoddCindy <cindodcindy@gmail.com>
2020-08-21 08:14:56 +0700

    test firebase masih blm dapat token

---
+++ app/src/main/java/geekgarden/zonamitraproject/testfcm/MainActivity.java
@@ -0,0 +1,14 @@
+package geekgarden.zonamitraproject.testfcm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
